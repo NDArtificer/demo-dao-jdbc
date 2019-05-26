c1cde76ac298164b5f53808aea8b53900fb771a7
NDArtificer <the.artificer@hotmail.com>
2019-05-26 19:37:28 -0300

    Criação da classe Seller

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,65 +1,15 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Connection conn = null;
-		PreparedStatement st = null;
-		
-
-		try {
-
-
-			conn = DB.getConnection();
-			/*	st = conn.prepareStatement(
-					"INSERT INTO seller"
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+	"VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-
-			st.setString(1, "Carlos Oliveira");
-			st.setString(2, "Carlos@outlook.com");
-			st.setDate(3, new  java.sql.Date(sdf.parse("19/04/1996").getTime()));
-			st.setDouble(4, 3500.00);
-			st.setInt(5, 4);
-			
-			*/
-			st = conn.prepareStatement(
-					"DELETE FROM seller "
-					+"WHERE "
-					+"Id = ?");
-				st.setInt(1, 8);
-			
-			int linhas = st.executeUpdate();
-		}
-		catch (SQLException e) {
-
-			e.printStackTrace();
-
-		} 
-
-		finally {
-
-			DB.closeStatement(st);
-
-			DB.closeConnection();
-
-		}
-
-
-
-}
-}
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Denny", "denny@hotmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
+	}
+ }
